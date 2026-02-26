2d813d8f10a35e24ff9586f1c05e11ef626fcbd9
“Moisguille” <moigodnav@alum.us.es>
2026-02-26 21:47:21 +0100

    added data access

--- index-MGN.js
+++ index-MGN.js
@@ -1,19 +1,12 @@
-
-
-
 const dataPart1 = require('./Data/csvjson.json'); 
 const dataPart2 = require('./Data/csvjson2.json');
-
- allDataRaw = [...dataPart1, ...dataPart2];
-const dataClean = allDataRaw
-    .filter(object => object.country !== "---" );
+allDataRaw = [...dataPart1, ...dataPart2];
+const dataClean = getCleanData(allDataRaw);
 const targetCountry = "Argentina";
 
 const average = dataClean
     .filter(object => object.country === targetCountry)
     .map(object => parseFloat(object.score))
-    
-    // C. Calcular la media usando reduce
     .reduce((acc, current, index, array) => {
         acc += current;
         if (index === array.length - 1) {
@@ -29,3 +22,21 @@ console.log(`Calculated Average Score: ${average.toFixed(2)}`);
 
 module.exports = { average, targetCountry };
 module.exports = { dataClean };
+
+
+
+
+
+function getCleanData(raw) {
+    let lastValidCountry = "";
+    return raw
+        .filter(obj => obj.country !== "---")
+        .map(obj => {
+            if (obj.country && obj.country.trim() !== "") {
+                lastValidCountry = obj.country;
+            } else {
+                obj.country = lastValidCountry;
+            }
+            return obj;
+        });
+}
