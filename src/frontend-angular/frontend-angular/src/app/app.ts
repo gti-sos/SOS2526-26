6d2b44eaa7c55f2e647277b88288ac0092b4e660
“Moisguille” <moigodnav@alum.us.es>
2026-03-31 14:55:10 +0200

    fixed bug Angular

--- src/frontend-angular/frontend-angular/src/app/app.ts
+++ src/frontend-angular/frontend-angular/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TaskListComponent } from './components/task-list/task-list';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [TaskListComponent], // <--- QUITAMOS RouterOutlet y PONEMOS el tuyo
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css' // Asegúrate que este nombre también sea el corto
 })
-export class App {
-  protected readonly title = signal('frontend-angular');
+export class App { // O export class AppComponent
+  title = 'frontend-angular';
 }
