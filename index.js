6ab16e22474c0ca48f6308c9ee078f94531e2cbb
RicardoFernandez2004 <ricferrod@alum.us.es>
2026-04-01 18:25:37 +0200

    cambio

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // Importar los 3 módulos (usando import)
 import mgn_api2 from './src/back/index-MGN2.js';
-import rfr_api1 from './src/back/index-RFR.js';
+//import rfr_api1 from './src/back/index-RFR.js';
 import rfr_api2 from './src/back/index-RFR2.js';
 //import sdv_api from './src/back/index-SDV.js';
 import sdv_api2 from './src/back/index-SDV2.js';
@@ -24,7 +24,7 @@ app.use(cors());
 // Inicializar los 4 módulos pasándoles 'app'
 //mgn_api2(app);
 sdv_api2(app);
-rfr_api1(app);
+//rfr_api1(app);
 rfr_api2(app);
 
 // Ruta /about 
