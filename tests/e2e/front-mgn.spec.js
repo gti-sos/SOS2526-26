18520dc5b210d34ea754cb45ba352cd17637ef60
“Moisguille” <moigodnav@alum.us.es>
2026-04-04 00:08:51 +0200

    Modified test

--- tests/e2e/front-mgn.spec.js
+++ tests/e2e/front-mgn.spec.js
@@ -1,10 +1,8 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-
-// Al quitar el mode: 'serial', Playwright ejecutará esto en paralelo automáticamente.
 test.describe('Pruebas E2E - Recurso MGN (Rankings)', () => {
 
-    // Cargamos los datos iniciales
+
     test.beforeAll(async ({ request }) => {
         await request.get('/api/v2/national-team-rankings-per-years/loadInitialData');
     });
@@ -17,7 +15,7 @@ test.describe('Pruebas E2E - Recurso MGN (Rankings)', () => {
     test('Debe listar todos los recursos iniciales', async ({ page }) => {
         const tableRows = page.locator('table tbody tr');
         await expect(tableRows.first()).toBeVisible();
-        // Solo comprobamos que haya algo, no el número exacto, porque otro test podría estar añadiendo datos
+   
         expect(await tableRows.count()).toBeGreaterThan(0);
     });
 
@@ -31,7 +29,7 @@ test.describe('Pruebas E2E - Recurso MGN (Rankings)', () => {
     });
 
     test('Debe crear un nuevo recurso completo', async ({ page }) => {
-        // Usamos un nombre muy único para que no choque con los datos iniciales
+       
         await page.getByPlaceholder('País (ej: España)').fill('Wakanda');
         await page.getByPlaceholder('Año (ej: 2024)').fill('3030'); 
         await page.getByPlaceholder('Posición').fill('1');
@@ -64,25 +62,24 @@ test.describe('Pruebas E2E - Recurso MGN (Rankings)', () => {
     });
 
     test('Debe editar un recurso específico', async ({ page }) => {
-        // 1. Buscamos OTRO país distinto para editar, para que no coincida con el test de borrar
-        // IMPORTANTE: Cambia 'Francia' por un país real de tu loadInitialData
+       
         const rowToEdit = page.locator('table tbody tr', { hasText: 'Alemania' }).first();
         
         await expect(rowToEdit).toBeVisible();
 
-        // 2. Le damos a editar a esa fila en concreto
+       
         await rowToEdit.locator('button:has-text("Editar")').click();
         
         const inputPos = page.locator('input[type="number"]').first();
         
-        // 3. Esperamos a que el input se rellene
+       
         await expect(inputPos).not.toHaveValue('');
         
-        // Escribimos el valor único y guardamos
+       
         await inputPos.fill('888'); 
         await page.click('button:has-text("Guardar Cambios")');
         
-        // 4. Verificamos que el 888 se ha guardado y aparece en la tabla
+       
         await expect(page.locator('table')).toContainText('888');
     });
 });
